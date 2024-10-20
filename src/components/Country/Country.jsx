d958d6ec5cd8e6ba10635b60407e9904d81150d8
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2024-10-20 10:00:23 +0600

    Added mark as read or visited country

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,14 +1,33 @@
+import { useState } from "react";
 import "./Country.css";
-const Country = ({ country }) => {
+const Country = ({ country, handleVisitedCountry }) => {
   const { name, flags, population, area, cca3 } = country;
 
+  const [visited, setVisited] = useState(false);
+  const handleVisited = () => {
+    setVisited(!visited);
+  };
   return (
-    <div className="country">
-      <h3>Name: {name?.common}</h3>
+    <div className={`country  ${visited ? "visited" : "non-visited"}`}>
+      <h3 style={{ color: visited ? "purple" : "black" }}>
+        Name: {name?.common}
+      </h3>
       <img src={flags.png} alt="" />
       <p>Population: {population}</p>
       <p>Area: {area}</p>
       <p>Code {cca3}</p>
+      <button
+        onClick={() => handleVisitedCountry(country)}
+        style={{ marginRight: "10px" }}
+      >
+        Mark as visited
+      </button>
+      <button onClick={handleVisited}>{visited ? "Visited" : "Going"}</button>
+      {visited ? (
+        <h4>I have visited this country</h4>
+      ) : (
+        <h4>I want to visit</h4>
+      )}
     </div>
   );
 };
